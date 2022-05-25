71c7fe6bb0a8400c99ef811522fbbc27887e9124
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2022-05-25 00:33:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+        <h1 className='title'>Hiromi Akita</h1>
+        <p className='subtitle'>Work in progress, but we can be friends on LinkedIn :&#41;</p>
+        <nav>
+          <a href='https://www.linkedin.com/in/hiromiakita/' target='_blank' rel="noopener noreferrer" >
+            <FontAwesomeIcon icon={faLinkedin} color="#0077b5" size="lg"/>
+          </a>
+        </nav>
       </header>
     </div>
   );
